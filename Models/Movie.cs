60fd7c626edfd14e1bcfb90eae0c3580f3d3068e
Deyvid <ermesdavidg.a@gmail.com>
2023-03-10 15:53:16 -0600

    Agregar archivos de proyecto.

---
+++ Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieCrud.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+        public string? Género { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+document.addEventListener("DOMContentLoaded", () => {
+    let boton = document.getElementById("crearpdf");
+    let conainar = document.getElementById("contenedor");
+
+    boton.addEventListener("click", event => {
+        event.preventDefault();
+        boton.style.display = "none";
+        window.print();
+    }, false);
+
+    conainar.addEventListener("click", event => {
+        boton.style.display = "initial";
+    }, false);
+
+}, false);
